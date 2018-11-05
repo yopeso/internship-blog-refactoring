d8b37bb639b7ee47f6a9127016b246f285a6cbcd
Julien Carré <julienroquai@gmail.com>
2018-11-05 16:17:46 +0100

    maj

--- controller/frontend.php
+++ controller/frontend.php
@@ -1,6 +1,20 @@
 <?php
+require('./model/model.php');
 
-// ...
+function listPosts()
+{
+    $posts = getPosts();
+
+    require('./view/listPostsView.php');
+}
+
+function post()
+{
+    $post = getPost($_GET['id']);
+    $comments = getComments($_GET['id']);
+
+    require('./view/postView.php');
+}
 
 function addComment($postId, $author, $comment)
 {
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require('controller/controller.php');
+require('controller/frontend.php');
 
 if (isset($_GET['action'])) {
     if ($_GET['action'] == 'listPosts') {
@@ -8,6 +8,19 @@ if (isset($_GET['action'])) {
     elseif ($_GET['action'] == 'post') {
         if (isset($_GET['id']) && $_GET['id'] > 0) {
             post();
+        }
+        else {
+            echo 'Erreur : aucun identifiant de billet envoyé';
+        }
+    }
+    elseif ($_GET['action'] == 'addComment') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+                addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+            }
+            else {
+                echo 'Erreur : tous les champs ne sont pas remplis !';
+            }
         }
         else {
             echo 'Erreur : aucun identifiant de billet envoyé';
