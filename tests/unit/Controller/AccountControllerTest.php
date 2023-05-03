d8f2eb9f967f2298051f582d6421e6e661b9975f
Vitalie Lisnic <vitalie.lisnic@yopeso.com>
2023-05-03 12:30:37 +0300

    adds first test and adjust autoloading

---
+++ tests/unit/Controller/AccountControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace UnitTests\App\Controller;
+
+use PHPUnit\Framework\TestCase;
+
+class AccountControllerTest extends TestCase
+{
+    public function testInterfaceAccount()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testComment()
+    {
+        $this->assertTrue(true);
+    }
+}
